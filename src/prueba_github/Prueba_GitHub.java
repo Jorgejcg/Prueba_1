9efa0667abcbfb513c625609dc00977c9b7032b1
Jorge <Jorge@LAPTOP-5TH6FKT0>
2023-05-10 18:09:48 +0200

    Aquí está

---
+++ src/prueba_github/Prueba_GitHub.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prueba_github;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Prueba_GitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("¡Hola, holita!");
+    }
+    
+}
